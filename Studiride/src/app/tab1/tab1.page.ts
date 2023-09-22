eb13f8c169f82695704cbc0c70b2763f5e4ef0b2
Madery Romain <romain.madery92@gmail.com>
2023-09-22 15:22:48 +0200

    Add some features to sample app

--- Studiride/src/app/tab1/tab1.page.ts
+++ Studiride/src/app/tab1/tab1.page.ts
@@ -6,7 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
+  public alertButtons = [
+    {
+      text: 'Non',
+      cssClass: 'alert-button-cancel',
+    },
+    {
+      text: 'Oui',
+      cssClass: 'alert-button-confirm',
+    },
+  ];
+  items: String[] = ["Covoite 1", "Covoite 2", "Covoite 3", "Covoite 4", "Covoite 5", "Covoite 6", "Covoite 7"]
+  avatarUrl: string = 'https://www.w3schools.com/howto/img_avatar.png';
   constructor() {}
 
+  sendMessage(index: String) {
+    console.log("Send a message to covoiturage with index:", index);
+  }
 }
